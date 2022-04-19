5ca3ece77f2bc641a9124061a79692092e9d4576
mak art <makart@live.co.uk>
2022-04-18 21:44:37 -0700

    refactored script added meaningful log

--- wakeup.js
+++ wakeup.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import Chalk, { chalkStderr } from "chalk";
 import fs from "fs";
 
 import { chromium } from "playwright";
@@ -7,161 +8,94 @@ import { exit } from "process";
 
 let username = process.env.username;
 
-let baseURL = process.env.BASE_URL || "https://scdev2.aacn.org";
+let baseURL = process.env.BASE_URL || "https://scdev.aacn.org";
 
-console.log(`AACN welcomes you, ${username} !!!`);
+console.log(
+  Chalk.bold.bgGray(" AACN SCRIPT UTILS "),
+  Chalk.bgBlue(" WAKEUP SITES \n")
+);
 
-/* paths to wake up...
-   /education
-   /nursing-excellence
-   /certification
-   /certification/get-certified
-   /conferences-and-events
-   /clinical-resources
-   /store
-   /store/cart
-  */
+let arrOfPaths = [
+  "/",
+  "/education",
+  "/nursing-excellence",
+  "/certification",
+  "/certification/get-certified",
+  "/conferences-and-events",
+  "/clinical-resources",
+  "/store",
+  "/store/cart",
+];
+async function wakePage(page, path, timeout) {
+  console.log(
+    Chalk.bold.blueBright(`fetching ${Chalk.cyan(baseURL + path)} ....`)
+  );
+  let start = performance.now();
+  let totalTime;
+  await page
+    .goto(path, { timeout })
+    .then(async (response) => {
+      await response.finished();
+      let address = await response.serverAddr();
+      console.log("Response Status", response.statusText());
+      if (response.ok()) {
+        let timeTaken = performance.now() - start;
+        totalTime = Math.round(timeTaken);
+        console.log(Chalk.green(`Successfull wakeup of ${response.url()} !!!`));
+        console.log(`On Server ${address.ipAddress}`);
+        console.log(
+          ` ${response.url()}` +
+            " took " +
+            Chalk.bgMagenta(`${Math.floor(timeTaken)}s`) +
+            " to respond \n"
+        );
+      } else {
+        console.log(
+          Chalk.magentaBright(
+            `The server responded with a ${response.status()} \n`
+          )
+        );
+        console.log(Chalk.magenta(`${response.statusText()}`));
+      }
+    })
+    .catch((err) => {
+      console.log(
+        Chalk.redBright(
+          `ERROR trying to connect with ${baseURL + path}: \n`,
+          Chalk.redBright(err)
+        )
+      );
+    });
+  return totalTime;
+}
 
-async function runBrowser() {
+async function runBrowser(paths) {
+  const timeout = 180000;
   const browser = await chromium.launch();
-
-  const page = await browser.newPage({
+  const thispage = await browser.newPage({
     acceptDownloads: true,
     baseURL,
     bypassCSP: true,
   });
 
-  let start = performance.now();
+  let table = [];
+  for (let mypath of paths) {
+    console.log(Chalk.bgMagenta(" PATH:   ", mypath + " "));
+    let timeTaken = await wakePage(thispage, mypath, timeout);
+    table = [...table, { path: mypath, seconds: Math.round(timeTaken / 1000) }];
+  }
 
-  await page
-    .goto("/")
-    .then((response) => {
-      let timeTaken = performance.now() - start;
-
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("ERROR=>", err);
-      process.exit(1);
-    });
-  start = performance.now();
-  await page
-    .goto("/education")
-    .then((response) => {
-      // console.log(response);
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-    });
-
-  await page
-    .goto("/nursing-excellence")
-    .then((response) => {
-      // console.log(response);
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-    });
-
-  await page
-    .goto("/certification")
-    .then((response) => {
-      // console.log(response);
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-    });
-
-  await page
-    .goto("/certification/get-certified")
-    .then((response) => {
-      // console.log(response);
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-      start = timeTaken;
-    });
-
-  await page
-    .goto("/conferences-and-events")
-    .then((response) => {
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-    });
-  await page
-    .goto("/clinical-resources")
-    .then((response) => {
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-    });
-
-  await page
-    .goto("/store")
-    .then((response) => {
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-    });
-
-  await page
-    .goto("/store/cart")
-    .then((response) => {
-      let timeTaken = performance.now() - start;
-      console.log(`Successfull wakeup of ${response.url()} !!!`);
-      console.log(` ${response.url()} took ${timeTaken} to respond \n`);
-      start = timeTaken;
-    })
-    .catch((err) => {
-      console.log("UhOh!!", err);
-      process.exit(1);
-    });
-  return { browser, totaltime: start };
+  await browser.close();
+  return table;
 }
-runBrowser()
+
+runBrowser(arrOfPaths)
   .then((res) => {
-    res.browser.close();
-    console.log(`TOTAL TIME: ${res.totaltime}`);
+    console.log(Chalk.green("ALL PATHS VISITED SUCCESSFULLY!!"));
+    console.table(res);
     process.exit(0);
   })
   .catch((err) => {
     console.log("THIS SCRIPT ERRORED!! =>", err);
+    process.exit(1);
   });
